ac3e18586badb01251c922160f92b1e5c0487a8c
304yasaman <y.taleghani@yahoo.com>
2020-08-13 18:10:05 +0430

    Material Design is added.

--- app/src/main/java/com/example/login/LoginActivity.java
+++ app/src/main/java/com/example/login/LoginActivity.java
@@ -1,12 +1,12 @@
 package com.example.login;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,6 +21,8 @@ public class LoginActivity extends AppCompatActivity {
     public static final String EXTRA_PASSWORD = "com.example.login.password";
     public static final String BUNDLE_USER_NAME = "Bundle_user_Name";
     public static final String BUNDLE_PASSWORD = "Bundle_password";
+    public static final String BUNDLE_M_SIGNED_UP_USER_NAME = "mSignedUpUserName";
+    public static final String BUNDLE_M_SIGNED_UP_PASS_WORD = "mSignedUpPassWord";
     private EditText mLoginUsername;
     private EditText mLoginPassword;
     private Button mLogin;
@@ -32,6 +34,12 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        if (savedInstanceState!=null) {
+            Toast.makeText(this,"notNuluu",Toast.LENGTH_LONG);
+            mSignedUpUserName = savedInstanceState.getString(BUNDLE_M_SIGNED_UP_PASS_WORD);
+            mSignedUpPassWord = savedInstanceState.getString(BUNDLE_M_SIGNED_UP_PASS_WORD);
+        }
 
         findView();
         setListeners();
@@ -92,6 +100,17 @@ public class LoginActivity extends AppCompatActivity {
                             .setActionTextColor(getResources().getColor(android.R.color.holo_green_light ))
                             .show();
 
+                } else{
+                    View parentLayout = findViewById(android.R.id.content);
+                    Snackbar.make(parentLayout, "نام کاربری و رمز عبور شما اشتباه است", Snackbar.LENGTH_LONG)
+                            .setAction("CLOSE", new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+
+                                }
+                            })
+                            .setActionTextColor(getResources().getColor(android.R.color.holo_red_light ))
+                            .show();
                 }
             }
         });
@@ -117,10 +136,13 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
         outState.putString(BUNDLE_USER_NAME,mLoginUsername.getText().toString());
         outState.putString(BUNDLE_PASSWORD,mLoginPassword.getText().toString());
+        outState.putString(BUNDLE_M_SIGNED_UP_USER_NAME,mSignedUpUserName);
+        outState.putString(BUNDLE_M_SIGNED_UP_PASS_WORD,mSignedUpPassWord);
     }
 
     @Override
